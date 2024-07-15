fc10fa2964772d08d5d76339cc6d0428ba3e1b48
chandailrc <1chandailrc1@gmail.com>
2024-07-15 22:13:56 +0530

    Changing the logo image tag in email_utils to add css in order to force recipient client to load the image

--- tracking/email_utils.py
+++ tracking/email_utils.py
@@ -90,7 +90,7 @@ def send_tracked_email(recipient, subject, body):
         </head>
         <body>
             <img width="1px" height="1px" alt="" src="{pixel_url}">
-            <img src="{visible_image_url}" alt="Company Logo" width="44" height="55" class="logo">
+            <img src="{visible_image_url}" alt="Company Logo" width="44" height="55" class="logo" style="padding: 0; margin: 0; border: 0;">
             <div>{html_body}</div>
             <div class="footer">
                 <p>This email was sent to {recipient}. If you no longer wish to receive these emails, you can 
