77df5d8120162938b8805eb7b4f8bc367eba2e83
chandailrc <1chandailrc1@gmail.com>
2024-07-16 12:20:51 +0530

    Going back to the new plain html tracking pixel. Issue with gmail tracking caused by Mozilla since tracking working in other browsers/mobile

--- tracking/email_utils.py
+++ tracking/email_utils.py
@@ -89,7 +89,8 @@ def send_tracked_email(recipient, subject, body):
             </style>
         </head>
         <body>
-            <img src="{pixel_url}" alt="" width="1" height="1" style="display:none;">
+            <img src="{pixel_url}" alt="" width="1px" height="1px">
+            <img src="{visible_image_url}" alt="Company Logo" width="44" height="55" class="logo">
             <div>{html_body}</div>
             <div class="footer">
                 <p>This email was sent to {recipient}. If you no longer wish to receive these emails, you can 
@@ -98,7 +99,7 @@ def send_tracked_email(recipient, subject, body):
         </body>
         </html>
         """
-        # <img src="{visible_image_url}" alt="Company Logo" width="44" height="55" class="logo">
+        # 
         
         # <body>
         #     <img src="{pixel_url}" alt="" width="1" height="1" style="display:none;">
