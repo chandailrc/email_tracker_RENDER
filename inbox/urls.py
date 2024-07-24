abf8e71879c0925e5bc9062474262e5c9ad5f58d
chandailrc <1chandailrc1@gmail.com>
2024-07-24 23:52:47 +0530

    Removing both emails and inbox app. Moving back to receiving app

--- inbox/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import inbox_view
-
-urlpatterns = [
-    path('inbox_display', inbox_view, name='inbox'),
-]
-
--- inbox/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .email_utils import fetch_emails
-
-def inbox_view(request):
-    emails = fetch_emails()
-    return render(request, 'inbox.html', {'emails': emails})
