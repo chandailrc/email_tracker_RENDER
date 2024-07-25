c80fea5662a2f7dde7dbbb2e88458e3c6e18e04d
chandailrc <1chandailrc1@gmail.com>
2024-07-25 14:51:20 +0530

    Updated with CSRF exemption. Will include CSRF later

--- receiving/views.py
+++ receiving/views.py
@@ -11,11 +11,13 @@ def receive_email(request):
     process_incoming_email(raw_email)
     return HttpResponse('Email received and processed', status=200)
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def fetch_emails(request):
     new_emails_count = fetch_and_process_emails()
     return JsonResponse({'new_emails': new_emails_count})
 
+@csrf_exempt
 @require_http_methods(["GET"])
 def list_emails(request):
     emails = ReceivedEmail.objects.all().order_by('-received_at')
